415ce048829a73baa7927392a298a7c748ac8c2f
Ina Ricci <ina_ricci@hotmail.com>
2024-01-08 07:38:25 -0800

    Updatede the connect function to handle incoming data and console.log it for the player

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host:"10.0.0.214", // IP address here,
+    port: 50541   // PORT number here,
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Connecting ...");
+});
+
+  return conn;
+};
+
+connect();
+
