c3fa45c245c8cae0b39546a116852d439b5d9b68
Ina Ricci <ina_ricci@hotmail.com>
2024-01-12 12:00:07 -0800

    created the input module by creating input.js and moved the setupInput and handleUserInput functions in this this file

---
+++ input.js
@@ -0,0 +1,17 @@
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  // terminate the game when ctrl + c is pressed
+  if (key === '\u0003') {
+  process.exit();
+  }
+};
+
+module.exports = {setupInput}
--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const {connect} = require("./client.js");
+const {setupInput} = require("./input.js")
 
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-
-const handleUserInput = function (key) {
-  // terminate the game when ctrl + c is pressed
-  if (key === '\u0003') {
-  process.exit();
-  }
-};
+console.log("Connecting ...");
 
 connect();
 setupInput();
